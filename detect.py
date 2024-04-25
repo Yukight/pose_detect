24bc2e763ab3e91da812b8ba70354c49ff7bd645
gouqichun <525458513@qq.com>
2024-04-25 11:13:28 +0800

    update fall_predict image

--- detect.py
+++ detect.py
@@ -70,15 +70,11 @@ class Detector:
                 # model_pose.predict(frame, show=True, save=True)[0].show()
                 cv2.imshow('detect_img', detect_img)
                 cv2.waitKey(0)
-                # os.system('rm -r -f ./runs/pose/predict2')
+                os.system('rm -r -f ./runs/pose/predict2')
                 self.model_pose.predict(frame, save=True)
-                time.sleep(5)
                 os.system('git add .')
-                time.sleep(5)
                 os.system('git commit -m "update fall_predict image"')
-                time.sleep(5)
                 os.system('git push')
-                time.sleep(10)
                 # try:
                 #     l_x, l_y, r_x, r_y = Results[0].boxes.xyxy[0].tolist()  # 返回YOLO检测框的左上、右下坐标
                 # except:
