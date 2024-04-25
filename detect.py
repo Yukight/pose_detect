c9df30e22ed653223a183f514162ae285f7cae89
gouqichun <525458513@qq.com>
2024-04-25 21:26:15 +0800

    fix(fixed requirements.txt):

--- detect.py
+++ detect.py
@@ -78,7 +78,8 @@ class Detector:
                 os.system('git add .')
                 os.system('git commit -m "update fall_predict image"')
                 os.system('git push')
-                os.system('git clone https://github.com/Yukight/pose_detect.git')
+                os.system('git pull')
+                # os.system('git clone https://github.com/Yukight/pose_detect.git')
                 # try:
                 #     l_x, l_y, r_x, r_y = Results[0].boxes.xyxy[0].tolist()  # 返回YOLO检测框的左上、右下坐标
                 # except:
